910c93b4a9ce51e4822d95d1625749f38b9d81b8
m1macER <eruiz6@mail.sfsu.edu>
2021-09-01 19:51:52 -0700

    Added tf2. Currently figuring out how to add Object Detection API to Jetson.

--- humanTracking.py
+++ humanTracking.py
@@ -1,3 +1,11 @@
 import tensorflow as tf
 import numpy as np
-import cv2
+from PIL import Image
+import os
+from matplotlib import pyplot as plt
+from utils import label_map_util
+
+#if __name__ == "__main__":
+plt.plot([1])
+plt.ylabel('numbers')
+plt.show()    
