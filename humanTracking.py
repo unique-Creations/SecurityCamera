8b80a764c24aedd61f938b3f39909a72812c6ea0
unique-Creations <eruiz6@mail.sfsu.edu>
2021-09-26 14:52:04 -0700

    OpenCV now tested on Nano.

--- humanTracking.py
+++ humanTracking.py
@@ -1,17 +1,18 @@
-import tensorflow as tf
-import tensorflow_hub as hub
-import matplotlib as plt
-
-
-# For drawing onto the image.
-import numpy as np
-from PIL import Image
-from PIL import ImageColor
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageOps
+# import tensorflow as tf
+# #import tensorflow_hub as hub
+# import matplotlib as plt
+#
+#
+# # For drawing onto the image.
+# import numpy as np
+# from PIL import Image
+# from PIL import ImageColor
+# from PIL import ImageDraw
+# from PIL import ImageFont
+# from PIL import ImageOps
 # import cv2
-
-# For measuring the inference time.
-import time
+#
+# # For measuring the inference time.
+# import time
 import camera_stream
+
