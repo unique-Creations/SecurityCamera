3ba62f82570e75d0f3df9d1a1f1fc5ad7d5e9828
m1macER <eruiz6@mail.sfsu.edu>
2021-10-14 22:11:53 -0700

    All GPIO pins that will be used for panning will is tested using LEDs. Must be tested using relays for motors.

--- gpio_tester.py
+++ gpio_tester.py
@@ -10,17 +10,45 @@ simple GPIO tester
 
 import Jetson.GPIO as GPIO
 
-channel = 12
-
+left = 12
+right = 15
+up = 31
+down = 19
 if __name__ == "__main__":
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(left, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(right, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(up, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(down, GPIO.OUT, initial=GPIO.LOW)
     command = None
 
     while command != "q":
         command = input("Enter command (q to quit):")
-        if command == "on":
-            GPIO.output(channel, GPIO.HIGH)
-        else:
-            GPIO.output(channel, GPIO.LOW)
-    GPIO.cleanup(channel)
+        if command == "l":
+            GPIO.output(up, GPIO.HIGH)
+            GPIO.output(down, GPIO.LOW)
+            GPIO.output(left, GPIO.HIGH)
+            GPIO.output(right, GPIO.LOW)
+        elif command == "r":
+            GPIO.output(up, GPIO.LOW)
+            GPIO.output(down, GPIO.LOW)
+            GPIO.output(left, GPIO.LOW)
+            GPIO.output(right, GPIO.HIGH)
+        elif command == "u":
+            GPIO.output(up, GPIO.HIGH)
+            GPIO.output(down, GPIO.LOW)
+            GPIO.output(left, GPIO.LOW)
+            GPIO.output(right, GPIO.LOW)
+        elif command == "d":
+            GPIO.output(up, GPIO.LOW)
+            GPIO.output(down, GPIO.HIGH)
+            GPIO.output(left, GPIO.LOW)
+            GPIO.output(right, GPIO.LOW)
+    GPIO.output(left, GPIO.LOW)
+    GPIO.output(right, GPIO.LOW)
+    GPIO.output(up, GPIO.LOW)
+    GPIO.output(down, GPIO.LOW)
+    GPIO.cleanup(left)
+    GPIO.cleanup(right)
+    GPIO.cleanup(up)
+    GPIO.cleanup(down)
