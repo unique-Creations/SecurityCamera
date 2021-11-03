b7b7fb465c226eb5a33a518fc8b224f6796f1acb
m1macER <eruiz6@mail.sfsu.edu>
2021-11-02 20:31:05 -0700

    Implementation of motor controls

--- frame_data.py
+++ frame_data.py
@@ -1,5 +1,6 @@
 # TODO: finish implementation
 # cv2 for debugging
+
 from cv2 import circle
 
 
@@ -24,18 +25,23 @@ def get_categ_names(classes, scores, cat_idx):
     return objects
 
 
-def person_coordinates(score, image, boxes):
+def person_coordinates(score, image, boxes, center):
     # TODO: Associate coordinates with object
     # TODO: Get center coordinate of box
-    # TODO: get accurate center coordinate
-    width, height = image.shape[:2]
+    height, width = image.shape[:2]
     true_boxes = boxes[0][score[0] > .5]
-    for i in range(true_boxes.shape[0]):
-        y_min = true_boxes[i, 0] * height
-        x_min = true_boxes[i, 1] * width
-        y_max = true_boxes[i, 2] * height
-        x_max = true_boxes[i, 3] * width
-        box_width = x_max - x_min
-        box_height = y_max - y_min
-        cx, cy = (int(box_width // 2), int(box_height // 2))
-        circle(image, (cx, cy), 7, (255, 255, 255), -1)
+    if len(true_boxes) > 0:
+        for i in range(true_boxes.shape[0]):
+            y_min = true_boxes[i, 0] * height
+            x_min = true_boxes[i, 1] * width
+            y_max = true_boxes[i, 2] * height
+            x_max = true_boxes[i, 3] * width
+            box_width = x_max - x_min
+            box_height = y_max - y_min
+            if box_height == box_width:
+                cx, cy = (int(box_width // 2), int(box_height // 2))
+            else:
+                cy, cx = (int((x_max + y_min) / 2), int((y_max + x_min) / 2))
+            circle(image, (cx, cy), 6, (255, 0, 0), 2)
+            return cx, cy
+    return center
--- motor_controller.py
+++ motor_controller.py
@@ -17,11 +17,50 @@ y_range = (-90, 90)
 
 
 def signal_handler(sig, frame):
-
+    print("Exiting...")
     sys.exit()
 
 
-def center_person(args, objX, objY, centerX, centerY, vid):
+def center_person(objX, objY, centerX, centerY, scores, image, boxes):
     signal.signal(signal.SIGINT, signal_handler)
     time.sleep(2.0)
+    while True:
+        (h, w) = image.shape[:2]
+        centerX.value, centerY.value = (w // 2, h // 2)
+        person_loc = frame_data.person_coordinates(scores, image, boxes, (centerX.value, centerY.value))
+        ((objX.value, objY.value), rect) = person_loc
 
+
+def pid_process(output, p, i, d, per_coord, center_coord):
+    signal.signal(signal.SIGINT, signal_handler)
+
+    p = pid.PID(p.value, i.value, d.value)
+    p.initialize()
+
+    while True:
+        error = center_coord.value - per_coord.value
+        output.value = p.update(error)
+
+
+def in_range(val, start, end):
+    return start <= val <= end
+
+
+def set_servos(pan, tlt):
+    # signal trap to handle keyboard interrupt
+    signal.signal(signal.SIGINT, signal_handler)
+    # loop indefinitely
+    while True:
+        # the pan and tilt angles are reversed
+        pan_angle = -1 * pan.value
+        tilt_angle = -1 * tlt.value
+        # if the pan angle is within the range, pan
+
+        # TODO: Implement appropriate functions for movement
+        if in_range(pan_angle, x_range[0], x_range[1]):
+            # Pan camera
+            pass
+        # if the tilt angle is within the range, tilt
+        if in_range(tilt_angle, y_range[0], y_range[1]):
+            # tilt camera
+            pass
