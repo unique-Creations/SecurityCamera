4b1f0e772a0d809dc25ac08a5611e6c101f708f2
m1macER <eruiz6@mail.sfsu.edu>
2021-11-03 21:33:37 -0700

    Documention added to frame_data

--- frame_data.py
+++ frame_data.py
@@ -1,10 +1,14 @@
-# TODO: finish implementation
 # cv2 for debugging
-
 from cv2 import circle
 
 
 def person_counter(obj_list):
+    """
+    Counts the number of persons detected within the frame.
+
+    :param obj_list: array of objects detected as integers (1 or 0).
+    :return: number of persons detected in the frame
+    """
     count = 0
     for obj in obj_list:
         if obj == 1:
@@ -13,6 +17,14 @@ def person_counter(obj_list):
 
 
 def get_categ_names(classes, scores, cat_idx):
+    """
+    Uses object names detect by TensorFlow and returns an int array to distinguish person from other objects.
+
+    :param classes: array of classes detected
+    :param scores: score of classes detected
+    :param cat_idx: indexes of classes detected
+    :return: array of objects detected. 1 if person, else 0
+    """
     objects = []
     threshold = 0.6
     for index, value in enumerate(classes[0]):
@@ -25,9 +37,16 @@ def get_categ_names(classes, scores, cat_idx):
     return objects
 
 
-def person_coordinates(score, image, boxes, center):
-    # TODO: Associate coordinates with object
-    # TODO: Get center coordinate of box
+def person_coordinates(score, image, boxes, center: (float, float)):
+    """
+    Calculates center coordinates of the person boxes within the frame.
+
+    :param score: array of category scores
+    :param image: frame read from camera
+    :param boxes: 2D matrix of object boxes detected
+    :param center: center coordinates of frame
+    :return: center coordinate of person boxes or center coordinate if 0 objects present
+    """
     height, width = image.shape[:2]
     true_boxes = boxes[0][score[0] > .5]
     if len(true_boxes) > 0:
@@ -38,10 +57,14 @@ def person_coordinates(score, image, boxes, center):
             x_max = true_boxes[i, 3] * width
             box_width = x_max - x_min
             box_height = y_max - y_min
+
             if box_height == box_width:
                 cx, cy = (int(box_width // 2), int(box_height // 2))
             else:
-                cy, cx = (int((x_max + y_min) / 2), int((y_max + x_min) / 2))
+                cx, cy = (int((x_max + y_min) / 2), int((y_max + x_min) / 2))
+
+            # For debugging purposes
+            # Comment out on Jetson Nano
             circle(image, (cx, cy), 6, (255, 0, 0), 2)
             return cx, cy
     return center
